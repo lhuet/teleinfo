c46df42dc4ee0049f40748f1e9c096e64b623cfe
Laurent HUET <laurent.huet@gmail.com>
2013-12-29 23:30:39 +0100

    fix omission ;

--- nodejs/test_tarifbleu.js
+++ nodejs/test_tarifbleu.js
@@ -4,14 +4,14 @@ var util = require('util');
 var datalogger = function (data) {
   console.log(new Date());
   console.log(util.inspect(data));
-}
+};
 
 tarifbleu.tarifbleu('/dev/ttyAMA0', '00 * * * * *', datalogger);
 
 setInterval(function() {
   console.log('Papp : ' + tarifbleu.getPuissanceApparente() + ' VA');
   console.log('Intensité : ' + tarifbleu.getIntensite() + ' A');
-  console.log('Index compteur : ' + tarifbleu.getIndex() + ' Wh')
+  console.log('Index compteur : ' + tarifbleu.getIndex() + ' Wh');
 }, 2000);
 
 console.log('Lancement datalogger tarifbleu');
